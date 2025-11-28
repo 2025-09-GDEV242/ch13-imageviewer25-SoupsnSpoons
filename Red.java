269caaf3837a84ef534d3415b12bdd52cb8aa576
SoupsnSpoons <143634308+SoupsnSpoons@users.noreply.github.com>
2025-11-27 23:02:50 -0500

    Error
    We cannot test the red if we cannot add it to the imageViewer folder, and since we cannot figure out why we are not able to do so, we shall pause here until assistance comes our way.

---
+++ Red.java
@@ -0,0 +1,43 @@
+import java.awt.Color;
+
+/**
+ * An image filter to remove color from an image.
+ * 
+ * @author Michael Kölling and David J. Barnes.
+ * @version 1.0
+ */
+public class Red extends Filter
+{
+    /**
+     * Constructor for objects of class Red.
+     * @param name The name of the filter.
+     */
+    public Red(String name)
+    {
+        super(name);
+    }
+
+    /**
+     * Apply this filter to an image.
+     * 
+     * @param  image  The image to be changed by this filter.
+     */
+    public void apply(OFImage image)
+    {
+        int height = image.getHeight();
+        int width = image.getWidth();
+        for(int y = 0; y < height; y++) {
+            for(int x = 0; x < width; x++) {
+                Color pix = image.getPixel(x, y);
+                int avg = (pix.getRed() + pix.getGreen() + pix.getBlue()) / 3;
+                if(pix.getRed() > ((pix.getRed() + pix.getGreen() + pix.getBlue()) / 3)){
+                    image.setPixel(x, y, new Color(255, 0, 0));
+                }
+                else {
+                    image.setPixel(x, y, new Color(0, pix.getGreen(), pix.getBlue()));
+                }
+                //image.setPixel(x, y, new Color(avg, avg, avg));
+            }
+        }
+    }
+}
